9cd67705de3bd1e20b5d73202a52632ef084c309
Lam0509 <112493001+Lam0509@users.noreply.github.com>
2022-09-06 14:22:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let inputWidth = prompt('Nhập chiều rộng');
+let inputHeight = prompt('Nhập chiều cao');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let Area = width * height;
+document.write('Enter width: ' + width)
+document.write('<br>')
+document.write('Enter height: ' + height)
+document.write('<br>')
+document.write('Diện tích hình chữ nhật là ' + Area)
